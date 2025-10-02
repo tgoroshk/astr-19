00db0dccc1319e1ff3d564a8dfe8c99333f4dcc4
tgoroshk <tgoroshk@ucsc.edu>
2025-10-02 12:11:36 -0700

    Add files via upload

---
+++ cjp1.py
@@ -0,0 +1,15 @@
+#Prompt: Write a Python program to print out your full name, 
+#your preferred pronouns (optional), 
+# and two sentences about your favorite movie and your favorite food.
+
+def PrintHelloWorld():
+	print("My name is Thomas Goroshko")
+	print("my pronouns are he/him")
+	print("my favorite movie is Lord of the Rings: fellowship of the ring")
+	print("my favorite food is lamb")
+
+def main():
+	PrintHelloWorld()
+
+if __name__=="__main__":
+	main()
