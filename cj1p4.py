feb551e037c556d766ace36933fca2d3a2990317
Thomas G <thomasg@eduroam-169-233-217-53.ucsc.edu>
2025-10-15 17:33:06 -0700

    adding prompt 4 for coding journal 1 with comments

--- cj1p4.py
+++ cj1p4.py
@@ -1,4 +1,5 @@
 '''
+Prompt 4:
 Write a Python program that declares a class describing your favorite animal. 
 Have the data members of the class represent the following physical parameters of the animal: 
 length of the arms (float),
@@ -13,44 +14,34 @@ representing the physical characteristics of the animal.
 '''
 
 
-		#55 cm arm length 
-		#65 cm leg length
-		#2 eyes
-		#has a tail
-		#very furry
-
-
+# Declaring class describing my favorite animal
 class My_favorite_animal:
 
+	# sets values for different data members
 	def __init__(self, length_of_arms, length_of_legs,  number_of_eyes, tail_existence,
 		is_it_furry): 
 
+		#data members:
 		self.length_of_arms = float(length_of_arms)
 		self.length_of_legs = float(length_of_legs)
 		self.number_of_eyes = int(number_of_eyes)
 		self.tail_existence = bool(tail_existence)
 		self.is_it_furry = bool(is_it_furry)
 
+	# Member function that prints and describes data members:
 	def describe(self):
 		print(f"Snow leopards front legs can be around {self.length_of_arms} cm")
 		print(f"Their hind legs can be around {self.length_of_legs} cm")
 		print(f"They have {self.number_of_eyes} eyes")
-
-		if self.tail_existence:
-			print("Snow leopards have a very bushy tail")
-		else:
-			print("Snow leopards don't have a tail")
-
-		if self.is_it_furry:
-			print("Snow leopards are exceptionally furry to "
-				"deal with extreme cold temperatures")
-		else:
-			print("Snow leopards aren't furry")
-
+		print(f"Snow leopards have a very bushy tail, {self.tail_existence}")
+		print(f"Snow leopards are very furry, {self.is_it_furry}")
 
 def main():
+	#creates instance of My_favorite_animal class with specific values
 	snow_leopard = My_favorite_animal(length_of_arms=55, length_of_legs=65, number_of_eyes=2, 
 		tail_existence=True, is_it_furry=True)
+
+	#calls describe() function on snow_leopard
 	snow_leopard.describe()
 
 if __name__ == "__main__":
